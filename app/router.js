7a3182a3a469e216dddd8101d6040eea8bf09242
hh404 <hjw231_2003@sina.com>
2024-09-09 12:15:58 +0800

    [emberjs-router]: demo

--- app/router.js
+++ app/router.js
@@ -1,9 +1,31 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'router-emberjs/config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
 
-Router.map(function () {});
+  import EmberRouter from '@ember/routing/router';  
+  import config from './config/environment';  
+  
+  const Router = EmberRouter.extend({  
+    location: config.locationType,  
+    rootURL: config.rootURL,  
+    
+    routes: [  
+      {  
+        path: '/',  
+        redirect: '/home'  
+      },  
+      {  
+        path: '/home',  
+        route: 'home'  
+      },  
+      {  
+        path: '/about',  
+        route: 'about'  
+      }  
+    ]  
+  });  
+  
+  Router.map(function() {  
+    this.route('home');  
+    this.route('about');  
+  });  
+  
+  export default Router;
---
+++ app/routes/about.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';  
+  
+export default Route.extend({  
+  model() {  
+    // 这里可以返回数据模型给about界面  
+    return {};  
+  }  
+});
