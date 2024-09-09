d061d6b62f2156693b91a7365d0feebeab427b24
hh404 <hjw231_2003@sina.com>
2024-09-09 13:11:12 +0800

    [ember-js]: second

--- app/router.js
+++ app/router.js
@@ -1,31 +1,14 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
 
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
 
-  import EmberRouter from '@ember/routing/router';  
-  import config from './config/environment';  
-  
-  const Router = EmberRouter.extend({  
-    location: config.locationType,  
-    rootURL: config.rootURL,  
-    
-    routes: [  
-      {  
-        path: '/',  
-        redirect: '/home'  
-      },  
-      {  
-        path: '/home',  
-        route: 'home'  
-      },  
-      {  
-        path: '/about',  
-        route: 'about'  
-      }  
-    ]  
-  });  
-  
-  Router.map(function() {  
-    this.route('home');  
-    this.route('about');  
-  });  
-  
-  export default Router;
+Router.map(function() {
+  this.route('home');
+  this.route('about');
+});
+
+export default Router;
